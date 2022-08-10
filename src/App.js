364c9907e86d67993b3269c53efabcb7a284bfaf
simmy varghese <simmyvarghese5@gmail.com>
2022-08-10 13:51:05 -0700

    Updated App.js

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
           <Route path='/about' element={<About/>} />
           <Route path='/projects' element={<Projects/>} />
           <Route path='/hireMe' element={<HireMe/>} />
+          <Route path='*' element={<Home/>} />
         </Routes>
     </Router>
     )
