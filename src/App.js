4e75e80ab5718ed1c0d845915b5f44547941ad71
simmy varghese <simmyvarghese5@gmail.com>
2022-08-10 21:21:40 -0700

    fix

--- src/App.js
+++ src/App.js
@@ -16,9 +16,9 @@ function App() {
         <Routes>  
           <Route path='/' element={<Home/>} />
           <Route path='/home' element={<Home/>} />
-          <Route path='about'  element={<About/>} />
-          <Route path='projects' element={<Projects/>} />
-          <Route path='hireMe' element={<HireMe/>} />
+          <Route path='/about'  element={<About/>} />
+          <Route path='/projects' element={<Projects/>} />
+          <Route path='/hireMe' element={<HireMe/>} />
         </Routes>
      </Router>
     )
