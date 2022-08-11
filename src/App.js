c72be8e2ea78274dca8189eab0bd4093ad8f58aa
simmy varghese <simmyvarghese5@gmail.com>
2022-08-10 19:22:28 -0700

    Added # to routes

--- src/App.js
+++ src/App.js
@@ -15,10 +15,10 @@ function App() {
         <Navbar/>
         <Routes>  
           <Route path='/' element={<Home/>} />
-          <Route path='/react-portfolio/home' element={<Home/>} />
-          <Route path='/react-portfolio/about'  element={<About/>} />
-          <Route path='/react-portfolio/projects' element={<Projects/>} />
-          <Route path='/react-portfolio/hireMe' element={<HireMe/>} />
+          <Route path='/react-portfolio/#/home"' element={<Home/>} />
+          <Route path='/react-portfolio/#/about'  element={<About/>} />
+          <Route path='/react-portfolio/#/projects' element={<Projects/>} />
+          <Route path='/react-portfolio/#/hireMe' element={<HireMe/>} />
         </Routes>
      </Router>
     )
